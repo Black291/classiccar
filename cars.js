f42092958150d0483cd995cd9f86cf3f56905ba5
Black291 <48406380+Black291@users.noreply.github.com>
2020-09-08 13:23:28 +0100

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,10 @@
+document.querySelector('.hamburger-menu').addEventListener('click',() => [
+    document.querySelector('.container-fluid').classList.toggle("change")
+]);
+
+setTimeout(() => [
+    documet.querySelector("html").style.scrollBehavior = "unset"
+], 1000
+
+);
+// document.querySelector('.menu-item').addEventListener('click',() =>).toggle;
